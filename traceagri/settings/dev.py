92ec55c75c977c41a8fcfee1f33453100aa86f3f
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-09-15 01:00:49 +0000

    aa

--- traceagri/settings/dev.py
+++ traceagri/settings/dev.py
@@ -2,3 +2,5 @@ import os
 import sys
 
 from .base import *
+GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH', '/opt/homebrew/opt/gdal/lib/libgdal.dylib')
+GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH', '/opt/homebrew/opt/geos/lib/libgeos_c.dylib')
