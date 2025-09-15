4b8f347911461719704a76101e758b52b5aa6f8c
ogahsergesergehoo <sergeleo14@yahoo.fr>
2025-09-15 01:29:15 +0000

    aa

--- traceagri/settings/base.py
+++ traceagri/settings/base.py
@@ -18,7 +18,7 @@ from django.contrib import messages
 from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
